bd978d4d148b1e9a21faa7f835f7b8270e67c781
Demilade30 <divine.akinjiyan@gmail.com>
2023-05-02 10:27:50 -0500

    added oss.c and user.c

---
+++ user.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <semaphore.h>
+#include <fcntl.h>
