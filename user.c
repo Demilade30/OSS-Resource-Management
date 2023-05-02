9832723c2e249e7a17b9662638ebe4bbdad715ce
Demilade30 <divine.akinjiyan@gmail.com>
2023-05-02 10:46:43 -0500

    updated user.c

--- user.c
+++ user.c
@@ -10,3 +10,34 @@
 #include <sys/shm.h>
 #include <semaphore.h>
 #include <fcntl.h>
+#define margin 10
+#define bounds 500
+
+struct timer {
+	unsigned int seconds;
+	unsigned int nanoseconds;
+};
+
+struct resources {
+	unsigned int maxAmount;
+	unsigned int available;
+	unsigned int request;
+	unsigned int allocation;
+	unsigned int release;
+	unsigned int requestArray[18];
+	unsigned int allocationArray[18];
+	unsigned int releaseArray[18];
+	int shared;
+};
+
+int errno;
+int index;
+int *shmChild;
+int *shmTerminate;
+char errmsg[200];
+struct timer *shmTime;
+struct resources *shmResources;
+pid_t pid;
+sem_t *semDead;
+sem_t *semTerminate;
+sem_t *semChild;
